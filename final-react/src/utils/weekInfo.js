1ad1012f72368d7b7cedab1e428444a49887b6a8
Nicholas Muller <100811519+queoui@users.noreply.github.com>
2023-05-30 12:00:37 -0700

    Update weekInfo.js

--- final-react/src/utils/weekInfo.js
+++ final-react/src/utils/weekInfo.js
@@ -1,14 +1,17 @@
 async function getWeekInfo(season, week) {
   try {
+    const nflUrl = '/api/nfl/odds/json/ScoresByWeek/'
+      .concat(season)
+      .concat('REG/')
+      .concat(week);
+    const apikey = import.meta.env.VITE_API_KEY;
+
     let response = await fetch(
-      'https://cors-anywhere.herokuapp.com/https://api.sportsdata.io/api/nfl/odds/json/ScoresByWeek/'
-        .concat(season)
-        .concat('REG/')
-        .concat(week),
+      nflUrl,
 
       {
         headers: {
-          'Ocp-Apim-Subscription-Key': 'efb56e725ab0490c89730b91c0d7bb60',
+          'Ocp-Apim-Subscription-Key': apikey,
         },
       }
     );
