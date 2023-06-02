4ec8ab16e00785c2803c9f146a0c43bd5d414397
Rajashree Pethkar <rpethkar@pdx.edu>
2023-06-02 16:47:01 -0700

    Added axios to use API calls

--- final-react/src/utils/gameStats.jsx
+++ final-react/src/utils/gameStats.jsx
@@ -1,19 +1,15 @@
+import axios from "axios";
+
+const nflUrl = "/api/nfl/odds/json/TeamGameStats/2022REG/";
+const headers = {
+  "Ocp-Apim-Subscription-Key": import.meta.env.VITE_API_KEY,
+  "Content-Type": "application/json",
+};
 
 async function getTeamGameStats(week) {
   try {
-    const nflUrl = '/api/nfl/odds/json/TeamGameStats/2022REG/';
-    const apikey = import.meta.env.VITE_API_KEY
-    let response = await fetch(
-      nflUrl.concat(
-        week
-      ),
-      {
-        headers: {
-          'Ocp-Apim-Subscription-Key': apikey,
-        },
-      }
-    );
-    return await response.json();
+    const response = await axios.get(nflUrl.concat(week), { headers });
+    return response.data;
   } catch (err) {
     console.error(err);
   }
--- final-react/src/utils/seasonStats.jsx
+++ final-react/src/utils/seasonStats.jsx
@@ -1,16 +1,15 @@
+import axios from "axios";
+
+const nflUrl = "/api/nfl/odds/json/TeamSeasonStats/2022REG";
+const headers = {
+  "Ocp-Apim-Subscription-Key": import.meta.env.VITE_API_KEY,
+  "Content-Type": "application/json",
+};
+
 async function getSeasonStats(week) {
   try {
-    const nflUrl = '/api/nfl/odds/json/TeamSeasonStats/2022REG'
-    const apikey = import.meta.env.VITE_API_KEY
-    let response = await fetch(
-      nflUrl,
-      {
-        headers: {
-          'Ocp-Apim-Subscription-Key': apikey,
-        },
-      }
-    );
-    return await response.json();
+    const response = await axios.get(nflUrl, { headers });
+    return response.data;
   } catch (err) {
     console.error(err);
   }
