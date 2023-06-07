25bcbea909443109c4ef9285e17176d88750b0c8
mu11ern <mu11ern@yahoo.com>
2023-06-06 17:04:33 -0700

    deploy

--- final-react/vite.config.js
+++ final-react/vite.config.js
@@ -1,25 +1,24 @@
 // vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { loadEnv } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { loadEnv } from 'vite';
 
-const env = loadEnv("development", process.cwd(), "");
+const env = loadEnv('development', process.cwd(), '');
 
 export default defineConfig({
-  base: "/front-end-final/",
+  // base: '/front-end-final/',
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
+      '/api': {
         target: env.VITE_NFL_URL,
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
       },
     },
     debug: true,
   },
 });
-
 
 // References
 // https://vitejs.dev/config/
